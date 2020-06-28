dbdb37e2cb37695dcf7bc40aa0146ab02979b731
Hồ Công Huynh <conghuynhho12@gmail.com>
2020-06-28 22:33:13 +0700

    Change the routeConfig.cs to the default

--- WebCNPM/App_Start/RouteConfig.cs
+++ WebCNPM/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebCNPM
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "DoiBong", action = "MyIndex", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
